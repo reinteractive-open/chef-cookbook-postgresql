6c01082bc6bab04481bb51eea00d2da7c16bddc0
Glenn Davy <glenn@davy.net.au>
2013-02-19 23:38:23 +1100

    modify pg to use official pg .deb repos instead of ppa

--- recipes/package.rb
+++ recipes/package.rb
@@ -7,10 +7,29 @@
 # All rights reserved - Do Not Redistribute
 #
 
-execute "add-apt-repository ppa:pitti/postgresql" do
+package wget do;end
+
+execute "get pg key" do
+  wget -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 end
-execute "apt-get update" do
+
+#TODO - fix this for correct lsb release
+execute "add repository" do
+  command 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main"'
 end
+
+execute "pin version" do
+  read -r -d '' PIN << EOF
+  Package: *
+  Pin: release o=apt.postgresql.org
+  Pin-Priority: 500
+  EOF
+  echo $PIN > /etc/apt/preferences.d/pgdg.pref
+end
+
+execute "apt-get update" do;end
+execute "sudo apt-get install pgdg-keyring"
+
 package "postgresql-9.2"
 package "postgresql-client-9.2"
 package "postgresql-contrib-9.2"
