3d0807f6fe9f9b0420e638fc4bd93749fa5c21ed
Glenn Davy <glenn@davy.net.au>
2013-02-20 09:16:34 +1100

    fixes to pg::pagacke

--- recipes/package.rb
+++ recipes/package.rb
@@ -7,27 +7,22 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package wget do;end
+package "wget"
 
 execute "get pg key" do
-  wget -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+  command 'wget -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -'
 end
 
 #TODO - fix this for correct lsb release
 execute "add repository" do
-  command 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main"'
+  command 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 end
 
-execute "pin version" do
-  read -r -d '' PIN << EOF
-  Package: *
-  Pin: release o=apt.postgresql.org
-  Pin-Priority: 500
-  EOF
-  echo $PIN > /etc/apt/preferences.d/pgdg.pref
+cookbook_file "/etc/apt/preferences.d/pgdg.pref" do
+  source "pg-pref"
 end
 
-execute "apt-get update" do;end
+execute "apt-get update" 
 execute "sudo apt-get install pgdg-keyring"
 
 package "postgresql-9.2"
