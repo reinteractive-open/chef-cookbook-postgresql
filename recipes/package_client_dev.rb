b354a1845661fbe9327be7a0db048b50ef63e78d
Glenn Davy <glenn@davy.net.au>
2013-05-04 09:43:44 +1000

    add client dev package

---
+++ recipes/package_client_dev.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: postgres-db-master
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "postgresql::package_repository"
+
+package "libpq-dev"
